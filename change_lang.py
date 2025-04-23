64c8b3c81052ad04b69f08e61b8fc04b8a8e4ff9
Lucas Li <2385014552@qq.com>
2025-04-23 11:21:49 +0800

    refactor: 添加检测和切换 Caps Lock 键的功能

--- change_lang.py
+++ change_lang.py
@@ -1,7 +1,23 @@
 import win32api
 import win32gui
 from win32con import WM_INPUTLANGCHANGEREQUEST
+import ctypes
 
+# 定义常量
+VK_CAPITAL = 0x14  # Caps Lock 键的虚拟键码
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP = 0x0002
+
+def is_caps_lock_on():
+    """检测 Caps Lock 键是否开启"""
+    return bool(ctypes.WinDLL('user32', use_last_error=True).GetKeyState(VK_CAPITAL))
+
+def toggle_caps_lock():
+    """切换 Caps Lock 键的状态"""
+    if is_caps_lock_on():
+        # 如果 Caps Lock 开启，模拟按下并释放 Caps Lock 键
+        ctypes.WinDLL('user32', use_last_error=True).keybd_event(VK_CAPITAL, 0, KEYEVENTF_EXTENDEDKEY, 0)
+        ctypes.WinDLL('user32', use_last_error=True).keybd_event(VK_CAPITAL, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0)
 
 def change_language(language="EN"):
     """
@@ -16,9 +32,9 @@ def change_language(language="EN"):
     """
     获取键盘布局
     """
-    im_list = win32api.GetKeyboardLayoutList()
-    im_list = list(map(hex, im_list))
-    print(im_list)
+    # im_list = win32api.GetKeyboardLayoutList()
+    # im_list = list(map(hex, im_list))
+    # print(im_list)
     
     hwnd = win32gui.GetForegroundWindow()
     language = LANGUAGE.get(language)
@@ -28,6 +44,7 @@ def change_language(language="EN"):
         0,
         language
     )
+    toggle_caps_lock()
     return result == 0
 
 if __name__ == '__main__':
